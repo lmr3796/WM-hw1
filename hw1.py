99ba6db3a8384904590bc5adb24080d5d68257e9
lmr3796 <lmr3796joseph@gmail.com>
2013-04-11 21:10:50 +0800

    Convert raw queries into query vector

--- hw1.py
+++ hw1.py
@@ -17,15 +17,22 @@ class InvertedIndex:
         self.lines = inv_idx.readlines()
         self.vocab = {'%s_%s' % (line.split()[0], line.split()[1]): {'range': (line_cnt, line_cnt + int(line.split()[2])),'idf': idf(int(line.split()[2])) }
                 for line_cnt, line in enumerate(self.lines) if len(line.split()) == 3 and int(line.split()[2]) > 0}
+        return
 
-
-class Query:
+class RawQuery:
     def __init__(self, topic):
         self.title = topic.find('title').text
         self.number = topic.find('number').text[-3:]
         self.question = topic.find('question').text.strip()
         self.narrative = topic.find('narrative').text.strip()
         self.concepts = topic.find('concepts').text.strip().replace(u'、', ' ').replace(u'。', ' ').split()
+        return
+
+class QueryVector:
+    def __init__(self, query, index):
+        raw_query_string = query.question + query.narrative + ' '.join(query.concepts)
+        self.vector = create_ngram(raw_query_string)
+        return
 
 def create_ngram(raw_query_string, n=MAX_NGRAM, raw=False):
     create_ngram_cmd = '%s/create-ngram -vocab %s -tmp . -n %d' % (config['tool_dir'], config['vocab'], n)
@@ -34,21 +41,12 @@ def create_ngram(raw_query_string, n=MAX_NGRAM, raw=False):
             shell=True).splitlines()
     return raw_result if raw else {line.split()[0]: line.split()[1] for line in raw_result}
 
-def similarity(query, doc_id):
+def similarity(qv, doc_id):
     return
-
-def process_query(query, index):
-    raw_query_string = query.question + query.narrative + ' '.join(query.concepts)
-    query_bigram = create_ngram(raw_query_string)
-    return
-
-def create_vector():
-    return 
 
 def main():
     # process arguments
-    (   
-            config['query_file'],
+    (config['query_file'],
             config['ranked_list'],
             config['model_dir'],
             config['vocab'],
@@ -69,14 +67,12 @@ def main():
     with open(config['inverted_index']) as inv_idx:
         index = InvertedIndex(inv_idx)
 
-    '''
     # parse query file
     tree = ElementTree.parse(config['query_file'])
     root = tree.getroot()
-    query_list = [Query(query) for query in root.findall('topic')]
+    query_list = [RawQuery(query) for query in root.findall('topic')]
     for query in query_list:
-        process_query(query)
-    '''
+        print QueryVector(query, index).vector
 
     return 0
 
