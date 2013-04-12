87e01bb33a9d704fc889eb2ca4aa802f77d8b656
lmr3796 <lmr3796joseph@gmail.com>
2013-04-12 21:22:09 +0800

    Outputs unfound bigram as str.

--- hw1.py
+++ hw1.py
@@ -16,6 +16,7 @@ BASEFILE_NAME = {
         }
 
 config = {}
+vocab = {}
 
 idf = lambda n: config['doc_cnt_log'] - math.log10(n) 
 
@@ -35,33 +36,30 @@ class RawQuery:
         self.concepts = topic.find('concepts').text.strip().replace(u'、', ' ').replace(u'。', ' ').split()
         return
 
-class QueryVector:
-    @staticmethod
-    def create_ngram(raw_query_string, n=MAX_NGRAM, raw=False):
-        create_ngram_cmd = '%s/create-ngram -vocab %s -tmp . -n %d' % (config['tool_dir'], config['vocab_file'], n)
-        raw_result = subprocess.check_output('echo \'%s\' | %s' % (raw_query_string.encode('utf8'), create_ngram_cmd),
-                stderr=open('/dev/null', 'w'),
-                shell=True).splitlines()
-        return raw_result if raw else dict(map(lambda x: (x.split()[0], int(x.split()[1])), raw_result))
+def convert_bigram_to_str(bigram):
+    (a,b) = bigram.split('_')
+    return '%s%s' % (vocab['line'][int(a)], vocab['line'][int(b)])
 
-    def __init__(self, query, index):
-        raw_query_string = query.question + query.narrative + ' '.join(query.concepts)
-        self.vector = QueryVector.create_ngram(raw_query_string)
-        return
+def create_ngram(raw_query_string, n=MAX_NGRAM, raw=False):
+    create_ngram_cmd = '%s/create-ngram -vocab %s -tmp . -n %d' % (config['tool_dir'], config['vocab_file'], n)
+    raw_result = subprocess.check_output('echo \'%s\' | %s' % (raw_query_string.encode('utf8'), create_ngram_cmd),
+            stderr=open('/dev/null', 'w'),
+            shell=True).splitlines()
+    return raw_result if raw else dict(map(lambda x: (x.split()[0], int(x.split()[1])), raw_result))
 
 def process_query(query, index):
     sim = defaultdict(float)
-    qv = QueryVector(query, index)
-    for bigram, query_bigram_score in qv.vector.iteritems():    # for each bigram
+    raw_query_string = query.question + query.narrative + ' '.join(query.concepts)
+    qv = create_ngram(raw_query_string)
+    for bigram, query_bigram_score in qv.iteritems():    # for each bigram
         if not index.vocab.has_key(bigram):
-            print >> sys.stderr, 'Bigram "%s" not found in vocabulary' % bigram
+            print >> sys.stderr, 'Bigram %s("%s") not found in vocabulary' % (bigram, convert_bigram_to_str(bigram))
             continue
-        gram_index = index.vocab[bigram]
-        for i in gram_index['range'][1:]:   # for each file with such bigram
+        for i in index.vocab[bigram]['range'][1:]:   # for each file with such bigram
             (doc_id, tf) = index.lines[i].split()
             tf = int(tf)
             sim[doc_id] += tf * index.vocab[bigram]['idf'] * query_bigram_score
-        
+
     return map(lambda x: x[0], sorted(sim.iteritems(), key=lambda x: x[1], reverse=True)[:MAX_DOC_RETURN])
 
 def main():
@@ -86,8 +84,10 @@ def main():
         print >> sys.stderr, 'Processing inverted index, this should take few minutes...',
         index = InvertedIndex(f)
         print >> sys.stderr, 'done.'
-    #with open(config['model_dir'] + BASEFILE_NAME['vocab']) as f:
-    #    vocab = {word.strip(): line for line, word in enumerate(f.readlines())}
+
+    with open('%s/%s' % (config['model_dir'], BASEFILE_NAME['vocab'])) as f:
+        vocab['line'] = map(str.strip, f.readlines())
+        vocab['bigram'] = {word.strip(): line for line, word in enumerate(vocab['line'])}
 
     # parse query file
     tree = ElementTree.parse(config['query_file'])
@@ -97,11 +97,11 @@ def main():
     with open(config['ranked_list'], 'w') as ranked_list:
         for query in query_list:
             doc_ids = process_query(query, index)
-            print >> sys.stderr, 'Processing query %s...' % (query.number),
+            print >> sys.stderr, 'Processing query %s...' % (query.number)
             for doc_id in doc_ids:
                 file_name = file_list[int(doc_id)]
                 print >> ranked_list, query.number, file_name[file_name.rfind('/')+1:].lower()
-            print >> sys.stderr, 'done, %d documents retrieved.' % (len(doc_ids))
+            print >> sys.stderr, 'Query %s done, %d documents retrieved.' % (query.number, len(doc_ids))
 
     return 0
 
